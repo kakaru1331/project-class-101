dc3761db1eeeeb6cdf705943778364d211088b5b
kakaru <kakaru1331@gmail.com>
2021-03-22 21:57:24 +0900

    feat: show EmptyCart component when cart is empty

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
+import { selectCartItemsCount } from '../store/slices/cartSlice'
+import EmptyCart from '../components/EmptyCart'
 
 function Cart () {
+  const cartItemsCount = useSelector(selectCartItemsCount)
+
   return (
-    <h1>Cart</h1>
+    <main>
+      {cartItemsCount === 0
+        ? <EmptyCart />
+        : <h1>Cart</h1>
+      }
+    </main>
   )
 }
 

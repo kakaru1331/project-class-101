17443220c3d8ff9058bdf9253b2874391f207c2d
JeKim <kakaru@einz.co.kr>
2021-03-23 13:08:42 +0900

    feat: render Coupon and PriceSummary in Cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -4,16 +4,22 @@ import { useSelector } from 'react-redux'
 import { selectCartItemsCount } from '../store/slices/cartSlice'
 import EmptyCart from '../components/EmptyCart'
 import CartItemList from '../components/CartItemList'
+import Coupon from '../components/Coupon'
+import PriceSummary from '../components/PriceSummary'
 
 function Cart () {
   const cartItemsCount = useSelector(selectCartItemsCount)
 
   return (
     <main>
-      {cartItemsCount === 0
-        ? <EmptyCart />
-        : <CartItemList />
-      }
+      {cartItemsCount === 0 && <EmptyCart />}
+      {cartItemsCount > 0 &&
+      <React.Fragment>
+        <CartItemList />
+        <Coupon />
+        <PriceSummary />
+      </React.Fragment>
+       }
     </main>
   )
 }

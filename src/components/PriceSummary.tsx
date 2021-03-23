7a77edfbb7fedfa270d71f69369ba992b1dd3dfe
JeKim <kakaru@einz.co.kr>
2021-03-23 19:24:39 +0900

    feat: implement UI and feature of price summary

--- src/components/PriceSummary.tsx
+++ src/components/PriceSummary.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import { Container, Grid, Card, CardContent, Typography } from '@material-ui/core'
 import {
   RemoveCircleOutlineSharp as RemoveCircleOutlineSharpIcon,
   PauseCircleFilledSharp as PauseCircleFilledSharpIcon
 } from '@material-ui/icons'
+import { useSelector } from 'react-redux'
+
+import { selectCartItems } from '../store/slices/cartSlice'
+
+import { ICoupon } from '../services/couponService'
+
+interface IProps {
+  selectedCoupon: ICoupon | null
+}
 
 const useStyles = makeStyles({
   container: {
@@ -19,18 +28,59 @@ const useStyles = makeStyles({
   },
   priceMessage: {
     textAlign: 'center',
-    fontSize: '1.2rem',
+    fontSize: '0.9rem',
     display: 'inline-block'
   },
   removeCircle: {
     height: '20px',
     verticalAlign: 'text-bottom',
-    color: 'gray'
+    color: 'gray',
+    margin: '0 3px'
+  },
+  eqaulCircle: {
+    transform: 'rotate(90deg)',
+    height: '20px',
+    verticalAlign: 'text-bottom',
+    margin: '0 3px'
+  },
+  strong: {
+    fontSize: '1.2rem'
+  },
+  totalPrice: {
+    fontSize: '1.2rem',
+    color: 'red'
   }
 })
 
-function PriceSummary () {
+function PriceSummary (props: IProps) {
+  const { selectedCoupon } = props
   const classes = useStyles()
+  const cartItems = useSelector(selectCartItems)
+  const [productsPrice, setProductsPrice] = useState(0)
+  const [discounted, setDiscounted] = useState(0)
+
+  useEffect(() => {
+    const reducedPrice = cartItems.reduce((acc, cur) => {
+      if (!cur.cartInfo.checked) {
+        return acc
+      }
+
+      return acc + (cur.productInfo.price * cur.cartInfo.amount)
+    }, 0)
+
+    setProductsPrice(reducedPrice)
+
+    if (selectedCoupon) {
+      if (productsPrice && selectedCoupon.type === 'rate' && selectedCoupon?.discountRate) {
+        const sale = Math.floor(productsPrice * (selectedCoupon.discountRate / 100))
+        setDiscounted(sale)
+      } else if (selectedCoupon.type === 'amount' && selectedCoupon?.discountAmount) {
+        setDiscounted(selectedCoupon.discountAmount)
+      }
+    } else {
+      setDiscounted(0)
+    }
+  }, [cartItems, selectedCoupon])
 
   return (
     <Container maxWidth="md">
@@ -38,13 +88,16 @@ function PriceSummary () {
         <Card className={classes.card}>
           <CardContent className={classes.cardContent}>
             <Typography variant="h5" className={classes.priceMessage}>
-              총 상품가격 <strong>{1000}</strong> 원
+              총 상품가격 <strong className={classes.strong}>{productsPrice.toLocaleString()}</strong> 원
             </Typography>
             <RemoveCircleOutlineSharpIcon className={classes.removeCircle} />
             <Typography variant="h5" className={classes.priceMessage}>
-              할인금액 <strong>{1000}</strong> 원
+              할인금액 <strong className={classes.strong}>{discounted.toLocaleString()}</strong> 원
             </Typography>
-            <PauseCircleFilledSharpIcon />
+            <PauseCircleFilledSharpIcon className={classes.eqaulCircle} />
+            <Typography variant="h5" className={classes.priceMessage}>
+              총 주문 금액 <strong className={classes.totalPrice}>{(productsPrice - discounted).toLocaleString()}</strong> 원
+            </Typography>
           </CardContent>
         </Card>
       </Grid>
